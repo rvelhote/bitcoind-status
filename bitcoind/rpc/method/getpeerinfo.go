e2af18ebbeeb91a00872511ed6d00abc7ec463fc
Ricardo Velhote <rvelhote@gmail.com>
2017-05-13 16:42:11 +0100

    Added some documentation based on bitcoin-cli help getpeerinfo

--- bitcoind/rpc/method/getpeerinfo.go
+++ bitcoind/rpc/method/getpeerinfo.go
@@ -37,30 +37,55 @@ type PeerInfoArgs struct {
 }
 
 type PeerInfo struct {
+	// Id is the Peer index
 	Id             int               `json:"id"`
+	// Addr is the ip address and port of the peer
 	Addr           string            `json:"addr"`
+	// AddrLocal is the local address
 	AddrLocal      string            `json:"addrlocal"`
+	// Services is an hexadecimal value that describes services offered by the peer
 	Services       string            `json:"services"`
+	// RelayTxes indicates whether peer has asked us to relay transactions to it
 	RelayTxes      bool              `json:"relaytxes"`
+	// LastSend is the time in seconds since epoch (Jan 1 1970 GMT) of the last send
 	LastSend       timestamp.Unix    `json:"lastsend"`
+	// LastRecv is the time in seconds since epoch (Jan 1 1970 GMT) of the last receive
 	LastRecv       timestamp.Unix    `json:"lastrecv"`
+	// BytesSent is the total bytes sent to this peer
 	BytesSent      uint64            `json:"bytessent"`
+	// BytesRecv is the total bytes received from this peer
 	BytesRecv      uint64            `json:"bytesrecv"`
+	// ConnTime is the connection time in seconds since epoch (Jan 1 1970 GMT)
 	ConnTime       timestamp.Unix    `json:"conntime"`
+	// TimeOffset is the time offset in seconds
 	TimeOffset     int               `json:"timeoffset"`
+	// PingTime is the ping to this peer (if available)
 	PingTime       float64           `json:"pingtime"`
+	// MinPing is the minimum observed ping time (if any at all)
 	MinPing        float64           `json:"minping"`
+	// Version is the peer version, such as 7001
 	Version        uint              `json:"version"`
+	// Subver is the string version of this peer
 	Subver         string            `json:"subver"`
+	// Inbound indicates if the connection is Inbound (true) or Outbound (false)
 	Inbout         bool              `json:"inbound"`
+	// AddNode indicates whether connection was due to addnode and is using an addnode slot
 	AddNode        bool              `json:"addnode"`
+	// StartHEight The starting height (block) of the peer
 	StartHeight    int               `json:"startingheight"`
+	// BanScore indicates the ban score for this peer
 	BanScore       uint              `json:"banscore"`
+	// SynchedHeaders is the last header we have in common with this peer
 	SynchedHeaders int               `json:"synced_headers"`
+	// SynchedBlocks is the last block we have in common with this peer
 	SynchedBlocks  int               `json:"synced_blocks"`
+	// Whitelisted indicates whether the peer is whitelisted
 	Whitelisted    bool              `json:"whitelisted"`
+	// BytesReceived is the total bytes received aggregated by message type
 	BytesReceived  PeerInfoBytesSent `json:"bytessent_per_msg"`
+	// BytesSent is the total bytes sent aggregated by message type
 	BytesSend      PeerInfoBytesSent `json:"bytesrecv_per_msg"`
+	// Humanize contains humanized data from the raw data in this struct
 	Humanize       PeerInfoHumanize
 }
 
