8444b11719e7a2b4e3cface0751736071d13292d
Ricardo Velhote <rvelhote@gmail.com>
2017-05-13 16:44:40 +0100

    Fixed some typos in the PeerInfo struct

--- bitcoind/rpc/method/getpeerinfo.go
+++ bitcoind/rpc/method/getpeerinfo.go
@@ -28,9 +28,9 @@ import (
 	"github.com/rvelhote/bitcoind-status/bitcoind/rpc"
 	"github.com/rvelhote/timestamp-marshal"
 	"log"
+	"math"
 	"net"
 	"strconv"
-	"math"
 )
 
 type PeerInfoArgs struct {
@@ -38,55 +38,79 @@ type PeerInfoArgs struct {
 
 type PeerInfo struct {
 	// Id is the Peer index
-	Id             int               `json:"id"`
+	Id int `json:"id"`
+
 	// Addr is the ip address and port of the peer
-	Addr           string            `json:"addr"`
+	Addr string `json:"addr"`
+
 	// AddrLocal is the local address
-	AddrLocal      string            `json:"addrlocal"`
+	AddrLocal string `json:"addrlocal"`
+
 	// Services is an hexadecimal value that describes services offered by the peer
-	Services       string            `json:"services"`
+	Services string `json:"services"`
+
 	// RelayTxes indicates whether peer has asked us to relay transactions to it
-	RelayTxes      bool              `json:"relaytxes"`
+	RelayTxes bool `json:"relaytxes"`
+
 	// LastSend is the time in seconds since epoch (Jan 1 1970 GMT) of the last send
-	LastSend       timestamp.Unix    `json:"lastsend"`
+	LastSend timestamp.Unix `json:"lastsend"`
+
 	// LastRecv is the time in seconds since epoch (Jan 1 1970 GMT) of the last receive
-	LastRecv       timestamp.Unix    `json:"lastrecv"`
+	LastRecv timestamp.Unix `json:"lastrecv"`
+
 	// BytesSent is the total bytes sent to this peer
-	BytesSent      uint64            `json:"bytessent"`
+	BytesSent uint64 `json:"bytessent"`
+
 	// BytesRecv is the total bytes received from this peer
-	BytesRecv      uint64            `json:"bytesrecv"`
+	BytesRecv uint64 `json:"bytesrecv"`
+
 	// ConnTime is the connection time in seconds since epoch (Jan 1 1970 GMT)
-	ConnTime       timestamp.Unix    `json:"conntime"`
+	ConnTime timestamp.Unix `json:"conntime"`
+
 	// TimeOffset is the time offset in seconds
-	TimeOffset     int               `json:"timeoffset"`
+	TimeOffset int `json:"timeoffset"`
+
 	// PingTime is the ping to this peer (if available)
-	PingTime       float64           `json:"pingtime"`
+	PingTime float64 `json:"pingtime"`
+
 	// MinPing is the minimum observed ping time (if any at all)
-	MinPing        float64           `json:"minping"`
+	MinPing float64 `json:"minping"`
+
 	// Version is the peer version, such as 7001
-	Version        uint              `json:"version"`
+	Version uint `json:"version"`
+
 	// Subver is the string version of this peer
-	Subver         string            `json:"subver"`
+	Subver string `json:"subver"`
+
 	// Inbound indicates if the connection is Inbound (true) or Outbound (false)
-	Inbout         bool              `json:"inbound"`
+	Inbound bool `json:"inbound"`
+
 	// AddNode indicates whether connection was due to addnode and is using an addnode slot
-	AddNode        bool              `json:"addnode"`
+	AddNode bool `json:"addnode"`
+
 	// StartHEight The starting height (block) of the peer
-	StartHeight    int               `json:"startingheight"`
+	StartHeight int `json:"startingheight"`
+
 	// BanScore indicates the ban score for this peer
-	BanScore       uint              `json:"banscore"`
+	BanScore uint `json:"banscore"`
+
 	// SynchedHeaders is the last header we have in common with this peer
-	SynchedHeaders int               `json:"synced_headers"`
+	SynchedHeaders int `json:"synced_headers"`
+
 	// SynchedBlocks is the last block we have in common with this peer
-	SynchedBlocks  int               `json:"synced_blocks"`
+	SynchedBlocks int `json:"synced_blocks"`
+
 	// Whitelisted indicates whether the peer is whitelisted
-	Whitelisted    bool              `json:"whitelisted"`
-	// BytesReceived is the total bytes received aggregated by message type
-	BytesReceived  PeerInfoBytesSent `json:"bytessent_per_msg"`
-	// BytesSent is the total bytes sent aggregated by message type
-	BytesSend      PeerInfoBytesSent `json:"bytesrecv_per_msg"`
-	// Humanize contains humanized data from the raw data in this struct
-	Humanize       PeerInfoHumanize
+	Whitelisted bool `json:"whitelisted"`
+
+	// BytesSentPerMsg is the total bytes sent aggregated by message type
+	BytesSentPerMsg PeerInfoBytesSent `json:"bytessent_per_msg"`
+
+	// BytesReceivedPerMsg is the total bytes received aggregated by message type
+	BytesReceivedPerMsg PeerInfoBytesSent `json:"bytesrecv_per_msg"`
+
+	// Humanize contains humanized data from the raw data in this
+	Humanize PeerInfoHumanize
 }
 
 type PeerInfoHumanize struct {
@@ -94,8 +118,8 @@ type PeerInfoHumanize struct {
 	BytesSent string
 	BytesRecv string
 	ConnTime  string
-	Services []string
-	PingTime string
+	Services  []string
+	PingTime  string
 }
 
 type PeerInfoBytesSent struct {
@@ -145,7 +169,6 @@ const (
 	// NODE_XTHIN means the node supports Xtreme Thinblocks
 	// If this is turned off then the node will not service nor make xthin requests
 	NODE_XTHIN uint64 = 1 << 4
-
 )
 
 // hostname fetches the hostname associated to the peer's ip address and sets it automatically (just call the func).
@@ -223,7 +246,7 @@ func GetPeerInfo(client *rpc.RPCClient) ([]PeerInfo, error) {
 		result[i].Humanize.BytesSent = humanize.Bytes(peer.BytesSent)
 		result[i].Humanize.ConnTime = humanize.Time(peer.ConnTime.Time)
 		result[i].Humanize.Services = Services(peer.Services)
-		result[i].Humanize.PingTime = strconv.FormatInt(int64(math.Ceil(peer.PingTime * 1000)), 10)
+		result[i].Humanize.PingTime = strconv.FormatInt(int64(math.Ceil(peer.PingTime*1000)), 10)
 	}
 
 	return result, nil
